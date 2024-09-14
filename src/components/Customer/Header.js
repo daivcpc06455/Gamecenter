8153293847e43255c829bb445a61b94c3dbd0426
NamLeeeee <hoangnamdmxgr@gmail.com>
2024-09-14 17:53:38 +0700

    Create Header.js

---
+++ src/components/Customer/Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="bg-primary text-white">
+      <h1 className="mb-0">Quản Lý Phòng Net</h1>
+    </header>
+  );
+}
+
+export default Header;
