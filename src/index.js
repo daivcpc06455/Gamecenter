28ef1ac22bdebb91a143b3cbc5aaf2bfe987b9ae
VuluanPC06483 <luanhvPC06483@fpt.edu.vn>
2024-09-14 12:25:15 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Thêm dòng này để import Bootstrap
+import './styles.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
