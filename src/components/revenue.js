90548296be40318cd5a16e278c089e1b97711e3f
NamLeeeee <hoangnamdmxgr@gmail.com>
2024-09-14 16:06:57 +0700

    Create revenue.js

---
+++ src/components/revenue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function revenue() {
+  return (
+    <div>
+      <h1>Thống Kê Doanh Thu</h1>
+      <p>Đây là trang hiển thị danh sách thống kê doanh thu.</p>
+    </div>
+  );
+}
+
+export default revenue;
