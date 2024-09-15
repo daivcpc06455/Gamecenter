0d8928ddd7f6eed6d0878f3099584542211bf612
chidai435 <daivcpc06544@fpt.edu.vn>
2024-09-15 14:37:36 +0700

    Create Banner.js

---
+++ src/components/Client/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Banner() {
+  return (
+    <section className="banner">
+      <img src="banner.jpg" alt="Banner Quảng Cáo" />
+    </section>
+  );
+}
+
+export default Banner;
