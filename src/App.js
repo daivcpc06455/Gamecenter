7a0cab7736676d0aac1f15339cc369151bd1fa03
kickboy1324 <kickboy1324@gmail.com>
2024-09-17 11:28:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import layout cho Admin và Client
 import AdminLayout from './components/Admin/Adminlayout';
-import ClientLayout from './components/CLient/Clientlayout'; 
-
-
-;
+import ClientLayout from './components/CLient/Clientlayout';
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Route dành cho Admin */}
-        <Route path="/admin/*" element={<AdminLayout />} />
+        <Route path="/*" element={<AdminLayout />} />
 
         {/* Route dành cho Client */}
-        <Route path="/*" element={<ClientLayout />} />
-        </Routes>
-       
+        <Route path="/Clientlayout/*" element={<ClientLayout />} />
+      </Routes>
     </Router>
-
   );
 }
 
